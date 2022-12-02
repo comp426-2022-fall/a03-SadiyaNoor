13ff9f0bd0ba951349a7dfd8c3c41cbce2f116e2
Sadiya Noor <sadiya@email.unc.edu>
2022-12-02 13:01:07 -0500

    done

--- bin/cli.js
+++ bin/cli.js
@@ -1,25 +1,8 @@
 #!/usr/bin/env node
-import minimist from 'minimist';
-import roll from '../lib/roll.js';
 
+import minimist from 'minimist';
+import { roll } from '../lib/roll.js';
 
 const args = minimist(process.argv.slice(2));
 
-
-var message_in_sides = 6;
-if (args.sides) {
-    message_in_sides = args.sides;
-}
-var message_in_dice = 2;
-if (args.dice) {
-    message_in_dice = args.dice;
-}
-var message_in_rolls = 1;
-if (args.rolls) {
-    message_in_rolls = args.rolls;
-}
-
-const message_out = roll(message_in_sides,message_in_dice,message_in_rolls);
-
-
-console.log(JSON.stringify(message_out));
+console.log(JSON.stringify(roll(args.sides || 6, args.dice || 2, args.rolls || 1)));
